fff029a99ab0cb8ab3a5acb72d3a93759abcaa09
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-10-24 12:36:14 -0300

    Updates

---
+++ test/integration/opaTests.qunit-dbg.js
@@ -0,0 +1,9 @@
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+  'use strict';
+
+  sap.ui.require(['sap/ui/demo/todo/test/integration/AllJourneys'], function () {
+    QUnit.start();
+  });
+});
